a58b84b2ce09934b54223bd0dc9a3cfd723f4fc7
Nikolai Luft <nikoluft92@gmail.com>
2024-05-28 15:05:48 +0200

    ready

--- lib/src/features/main_screen.dart
+++ lib/src/features/main_screen.dart
@@ -8,35 +8,72 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  final TextEditingController zipController = TextEditingController();
+  Future<String>? cityFuture;
+
   @override
   void initState() {
     super.initState();
-    // TODO: initiate controllers
+
+    zipController.addListener(() {
+      setState(() {
+        cityFuture = null;
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Center(
-          child: Column(
-            children: [
-              const TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(), labelText: "Postleitzahl"),
-              ),
-              const SizedBox(height: 32),
-              OutlinedButton(
-                onPressed: () {
-                  // TODO: implementiere Suche
-                },
-                child: const Text("Suche"),
-              ),
-              const SizedBox(height: 32),
-              Text("Ergebnis: Noch keine PLZ gesucht",
-                  style: Theme.of(context).textTheme.labelLarge),
-            ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: Column(
+              children: [
+                TextField(
+                  controller: zipController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Postleitzahl",
+                  ),
+                ),
+                const SizedBox(height: 32),
+                OutlinedButton(
+                  onPressed: () {
+                    // TODO: implementiere Suche
+                    setState(() {
+                      cityFuture = getCityFromZip(zipController.text);
+                    });
+                  },
+                  child: const Text("Suche"),
+                ),
+                const SizedBox(height: 32),
+                FutureBuilder<String>(
+                  future: cityFuture,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const CircularProgressIndicator();
+                    } else if (snapshot.hasError) {
+                      return Text(
+                        "Fehler: ${snapshot.error}",
+                        style: Theme.of(context).textTheme.labelLarge,
+                      );
+                    } else if (snapshot.hasData) {
+                      return Text(
+                        "Ergebnis: ${snapshot.data}",
+                        style: Theme.of(context).textTheme.labelLarge,
+                      );
+                    } else {
+                      return Text(
+                        "Ergebnis: Noch keine PLZ gesucht",
+                        style: Theme.of(context).textTheme.labelLarge,
+                      );
+                    }
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -45,22 +82,23 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   void dispose() {
-    // TODO: dispose controllers
+    zipController.dispose();
     super.dispose();
   }
 
   Future<String> getCityFromZip(String zip) async {
-    // simuliere Dauer der Datenbank-Anfrage
     await Future.delayed(const Duration(seconds: 3));
 
     switch (zip) {
       case "10115":
+      case "10997":
+      case "14195":
         return 'Berlin';
       case "20095":
         return 'Hamburg';
       case "80331":
         return 'München';
-      case "50667":
+      case "50765":
         return 'Köln';
       case "60311":
       case "60313":
